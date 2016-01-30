146c9f15b32e0615ae910721c725ddb38623ed9c
Eric Nation <eric.nation@gmail.com>
2016-01-30 14:21:36 -0700

    Add React Router

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = React.Router.Route;
+
 
 var Hello = React.createClass({
   render: function() {

9b543d63e20b9820735aff568d252ed9c69fdf8e
Eric Nation <eric.nation@gmail.com>
2016-02-11 22:21:54 -0700

    Add image store

--- src/components/topic.jsx
+++ src/components/topic.jsx
@@ -1,15 +1,34 @@
 var React = require('react');
+var Reflux = require('reflux');
+var ImageStore = require('../stores/image-store');
+var Actions = require('../actions');
 
 module.exports = React.createClass({
+	mixins: [
+		Reflux.listenTo(ImageStore, 'onChange')
+	],
+	getInitialState:function() {
+		return {
+			images: []
+		}
+	},
+	componentWillMount: function() {
+		Actions.getImages(this.props.params.id);
+	},
 	render: function() {
 		return  (
 			<div className="container">
 				<div className="row">
 					<div className="col-md-12">	
-						I am a topic with ID {this.props.params.id}
+						
 					</div>
 				</div>
 			</div>
 		)
+	},
+	onChange: function(event, images) {
+		this.setState({
+			images: images
+		})
 	}
-})
+});
